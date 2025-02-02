8207cbd191e48c930de92fe50aaf251b66c82796
Russell Gilmore <russellgilmore@live.com>
2025-02-02 13:33:01 -0500

    Test Site

---
+++ test-site/test.tf
@@ -0,0 +1,62 @@
+variable "project_name" {
+  description = "Set the project name."
+  type        = string
+  default     = "test-manual-red-775"
+}
+
+variable "region" {
+  description = "Set the appropriate AWS region."
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "apex_domain" {
+  description = "Set the apex domain."
+  type        = string
+  default     = "rag-space.com"
+}
+
+variable "record_name" {
+  description = "Set the record name."
+  type        = string
+  default     = "test.rag-space.com"
+}
+
+variable "enable_static_website" {
+  description = "Enable the creation of resources needed to support a secure and available static website."
+  type        = bool
+  default     = true
+}
+
+variable "website_path" {
+  description = "Set the path to the website content."
+  type        = string
+  default     = "site"
+}
+
+module "static-website" {
+  source = "../red-bucket"
+
+  project_name          = var.project_name
+  region                = var.region
+  apex_domain           = var.apex_domain
+  record_name           = var.record_name
+  enable_static_website = var.enable_static_website
+  website_path          = var.website_path
+}
+
+output "red_bucket_name" {
+  value = module.static-website.red_bucket_name
+}
+
+output "website_url" {
+  value = module.static-website.website_url
+}
+
+output "s3_url" {
+  value = module.static-website.s3_url
+}
+
+output "website_record" {
+  value = module.static-website.website_record
+}
